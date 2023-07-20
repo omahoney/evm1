ded526691f39db566bf80c1cf5373c1e21229ac4
Benedict O'Mahoney <benedict@benedict.com>
2023-07-19 21:11:01 -0700

    Created error with shadow function

--- contracts/contracts/Greeter.sol
+++ contracts/contracts/Greeter.sol
@@ -7,14 +7,19 @@ contract Greeter {
     string private meeting;
 
     // Constructor - run when contract is deployed
-    constructor(string memory _greeting) {
+    constructor(string memory _greeting, string memory _meeting) {
         greeting = _greeting;
-        meeting
+        meeting = _meeting;
     }
 
     // Read function (can be called without a transaction)
     function greet() public view returns (string memory) {
         return greeting;
+    }
+
+    // Read function (can be called without a transaction)
+    function meet() public view returns (string memory) {
+        return meeting;
     }
 
     // Write function (requires a transaction to be called)
